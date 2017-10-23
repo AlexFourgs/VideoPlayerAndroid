c225b091ba12ea49c7f62880b3de31ea24c4eafb
Alex.F <alexandre.fourgs@gmail.com>
2017-10-23 14:18:06 +0200

    Ajout du mini projet du prof

---
+++ VideoPlayer/app/src/main/java/com/delkappa/manos/videoplayer/MainActivity.java
@@ -0,0 +1,35 @@
+package com.delkappa.manos.videoplayer;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import android.net.Uri;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get a reference to the VideoView instance as follows, using the id we set in the XML layout.
+        VideoView vidView = (VideoView)findViewById(R.id.video);
+
+        // Add playback controls.
+        MediaController vidControl = new MediaController(this);
+        // Set it to use the VideoView instance as its anchor.
+        vidControl.setAnchorView(vidView);
+        // Set it as the media controller for the VideoView object.
+        vidView.setMediaController(vidControl);
+
+        // Prepare the URI for the endpoint.
+        String vidAddress = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri vidUri = Uri.parse(vidAddress);
+        // Parse the address string as a URI so that we can pass it to the VideoView object.
+        vidView.setVideoURI(vidUri);
+        // Start playback.
+        vidView.start();
+    }
+}
